ba126f03e0ebf377796cdd12994ef0210231642a
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-18 11:27:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,17 +1,15 @@
-class Student < User      # inherits from the User class
-  
-  def initialize        # initializes with an empty knowledge array 
+class Student < User
+
+  def initialize
     @knowledge = []
-  def 
-  
-  def learn(fact)           # takes in an argument of a string of knowledge and adds it to the student's knowledge array
-  @knowledge << fact 
-  end 
-  
-  def knowledge         # retrun the student's knowledge array
+  end
+
+  def learn(fact)
+    @knowledge << fact
+  end
+
+  def knowledge
     @knowledge
-  end 
-  
-  
+  end
   
 end
